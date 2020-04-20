8acba384f76069f4a8c6581e822c6909b409a66c
Iskak Mantyubetov <ayzik55@gmail.com>
2020-04-20 16:46:23 -0400

    Book Route Done

---
+++ server/api/book.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const {Book} = require('../db/models')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    const books = await Book.findAll()
+    res.json(books)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    const book = await Book.findByPk({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(book)
+  } catch (err) {
+    next(err)
+  }
+})
--- server/api/index.js
+++ server/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router()
 module.exports = router
 
 router.use('/users', require('./users'))
+router.use('/book', require('./book'))
 
 router.use((req, res, next) => {
   const error = new Error('Not Found')
