3392066a8c8e060ad6536d941bc4d33dcd6b4fc8
Franco Trelles <franco.trellesm@gmail.com>
2020-04-22 16:50:29 -0400

    Updating routes

--- server/api/author.js
+++ server/api/author.js
@@ -1,11 +1,25 @@
 const router = require('express').Router()
-const {Author} = require('../db/models')
+const {Author, Book} = require('../db/models')
 
 //Find all authors:
+
 router.get('/', async (req, res, next) => {
   try {
     const authors = await Author.findAll()
     res.json(authors)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get('/:id/books', async (req, res, next) => {
+  try {
+    const allAuthorBooks = await Author.findAll({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.json(allAuthorBooks)
   } catch (err) {
     next(err)
   }
