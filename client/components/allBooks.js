28a3c17dd2b4e677e61ac6cf256c478438df270b
Jared <usherjared@yahoo.com>
2020-04-23 00:11:24 -0400

    Added react/redux for all books, fixed issue with genre and id books route, added link and route for all books, changed main page h1 tag and the broswer tab to say Book-Shopper

---
+++ client/components/allBooks.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {fetchBooks} from '../store'
+
+class AllBooks extends React.Component {
+  componentDidMount() {
+    this.props.fetchBooks()
+  }
+  render() {
+    console.log('props ---->', this.props)
+    return (
+      <div>
+        <h1>Books:</h1>
+        <ul>
+          {this.props.books.map(book => {
+            return <li key={book.id}>{book.title}</li>
+          })}
+        </ul>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => ({
+  books: state.books
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchBooks: () => dispatch(fetchBooks())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllBooks)
---
+++ client/store/books.js
@@ -0,0 +1,44 @@
+import axios from 'axios'
+
+//Action Types:
+const GET_BOOKS = 'GET_BOOKS'
+//Not functional yet, placeholder for future features:
+const ADD_BOOK = 'ADD_BOOK'
+const REMOVE_BOOK = 'REMOVE_BOOK'
+
+//Action Creators:
+const getBooks = books => ({
+  type: GET_BOOKS,
+  books
+})
+//Place holder action creators for future functionality:
+const addBook = book => ({
+  type: ADD_BOOK,
+  book
+})
+const removeBook = book => ({
+  type: REMOVE_BOOK,
+  book
+})
+
+//Thunk Creators:
+export const fetchBooks = () => async dispatch => {
+  try {
+    const {data} = await axios.get('/api/book')
+    dispatch(getBooks(data))
+  } catch (error) {
+    return error
+  }
+}
+
+//Reducer:
+const books = []
+
+export default function booksReducer(state = books, action) {
+  switch (action.type) {
+    case GET_BOOKS:
+      return action.books
+    default:
+      return state
+  }
+}
