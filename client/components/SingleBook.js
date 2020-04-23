9d98af2c42d5dee50dc99a102937d50beec186f0
HagenGaryP <hagengaryp@gmail.com>
2020-04-23 14:01:17 -0400

    links work, need to fix singleBook author

--- client/components/SingleBook.js
+++ client/components/SingleBook.js
@@ -10,29 +10,37 @@ class SingleBook extends React.Component {
 
   render() {
     let book = this.props.book
-
+    let author = this.props.book.author
+    console.log(' >>> props >>>', this.props)
+    console.log('>>>> author >>>', author)
     return (
       <div>
         <main>
           <h2>Title: {book.title}</h2>
           <img src={book.imageUrl} />
-          <h3>Author: {book.author}</h3>
+          {/* <h3>Author: {author.firstName} {author.lastName} </h3> */}
           <h4>Genre: {book.genre}</h4>
         </main>
+        <div>
+          <br />
+          <Link to="/allBooks">
+            <h3>View all all books</h3>
+          </Link>
+        </div>
       </div>
     )
   }
 }
 
 const mapState = state => {
-  console.log('mapping state.book >>>>', state.book)
+  // console.log('mapping state.book >>>>', state.book)
   return {
     book: state.book
   }
 }
 
 const mapDispatch = (dispatch, ownProps) => {
-  console.log('this is dispatch to props >>>>', ownProps)
+  // console.log('this is dispatch to props >>>>', ownProps)
   return {
     getSingleBook: () => dispatch(fetchSingleBook(ownProps.match.params.id))
   }
