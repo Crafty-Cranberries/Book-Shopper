2b92f5adc6d41ab6fdf85995c04905f467abb848
Franco Trelles <franco.trellesm@gmail.com>
2020-04-23 11:25:59 -0400

    Added minimal parts of homepage

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import Button from 'react-bootstrap/Button'
 
 /**
  * COMPONENT
@@ -15,12 +16,18 @@ export const UserHome = props => {
         <div className="front-page-welcome">
           <div>
             <h1 className="front-display-text">
-              Buy the best books from all over
+              Buy The Best Books From <br />
+              Around The World
             </h1>
+            <p className="subtext">
+              Browse through our vibrant collection of books. Search by Author
+              or by Genre. <br />
+              Check out our clearance section for plenty of discounts!
+            </p>
           </div>
           <div className="front-page-buttons">
-            <button type="button">All Books</button>
-            <button type="button">All Authors</button>
+            <Button variant="outline-light button-margin">All Books</Button>
+            <Button variant="outline-light">All Authors</Button>
           </div>
         </div>
         <div className="placeholder" />
