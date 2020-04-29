a04dea7c5d6e7df8b99966c88b548469ad1db42b
HagenGaryP <hagengaryp@gmail.com>
2020-04-28 21:39:04 -0400

    renders all users

--- client/components/all-users.js
+++ client/components/all-users.js
@@ -2,27 +2,33 @@ import React from 'react'
 import {connect} from 'react-redux'
 import Card from 'react-bootstrap/Card'
 import {Link} from 'react-router-dom'
-import user from '../store/user'
+// import user from '../store/user'
+import {fetchAllUsers} from '../store/usersReducer'
 
 class AllUsers extends React.Component {
+  componentDidMount() {
+    this.props.getUsers()
+  }
+
   render() {
     return (
       <div>
-        <h1>Books:</h1>
-        {this.props.users.map(singleUser => {
-          return (
-            <Card key={singleUser.id} style={{width: '18rem'}}>
-              <Card.Body>
-                <Link to="/users">
-                  <Card.Title>
-                    Name: {singleUser.firstName} {singleUser.lastName}{' '}
-                  </Card.Title>
-                </Link>
-                <Card.Subtitle>Email: {singleUser.email}</Card.Subtitle>
-              </Card.Body>
-            </Card>
-          )
-        })}
+        <h1>List of all the users</h1>
+        {Array.isArray(this.props.users) &&
+          this.props.users.map(singleUser => {
+            return (
+              <Card key={singleUser.id} style={{width: '18rem'}}>
+                <Card.Body>
+                  <Link to="/users">
+                    <Card.Title>
+                      Name: {singleUser.firstName} {singleUser.lastName}{' '}
+                    </Card.Title>
+                  </Link>
+                  <Card.Subtitle>Email: {singleUser.email}</Card.Subtitle>
+                </Card.Body>
+              </Card>
+            )
+          })}
       </div>
     )
   }
@@ -31,5 +37,8 @@ class AllUsers extends React.Component {
 const mapStateToProps = state => ({
   users: state.users
 })
+const mapDispatch = dispatch => ({
+  getUsers: () => dispatch(fetchAllUsers())
+})
 
-export default connect(mapStateToProps)(AllUsers)
+export default connect(mapStateToProps, mapDispatch)(AllUsers)
